a44d868529528c41eeafcbf362340ae62fa20107
William Wise <williamwise@Will-Wise.local>
2017-08-08 07:36:23 +1000

    first commit

--- authenticate.php
+++ authenticate.php
@@ -2,7 +2,7 @@
 require_once '../vendor/autoload.php';
 
 define('ID_TOKEN', $_POST['token']);
-define('CLIENT_ID', '400338796931-rnmvtbfofqhsvfd8rvat5igq8h121v46.apps.googleusercontent.com');
+define('CLIENT_ID', '<your client id>');
 
 $client = new Google_Client(['client_id' => CLIENT_ID]);
 $payload = $client->verifyIdToken(ID_TOKEN);
@@ -17,7 +17,8 @@ if (!$payload) {
 }
 
 // Validate that the user is within the organisation
-if ($payload['hd'] !== 'siteminder.com') {
+// Optional...
+if ($payload['hd'] !== '<your gsuite company domain>') {
   echo json_encode(array(
     'status' => false,
     'body' => 'Invalid organisation'
--- data/setup.php
+++ data/setup.php
@@ -1,5 +1,11 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
+  /**
+   * Example request file.
+   * Make an HTTPS Post request to this file.
+   *
+   * Valid response: The JSON below
+   * Invalid response: A JSON object with a status and error MessageFormatter
+   */
   header('Content-Type: application/json');
   require_once '../authenticate.php'
 ?>
